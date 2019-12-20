1690b14b0512b8a0a4e987f09b82df08c064cbed
caimiao0714 <miao.cai@slu.edu>
2019-12-20 10:09:07 -0600

    update figure with geom_tile()

--- 04plot_concordance.R
+++ 04plot_concordance.R
@@ -20,12 +20,13 @@ d = data.table::fread("data/lk_risks.csv") %>%
 
 #title = "Concordance of four models for evaluating the risk of crash",
 #caption = "Darker color indicates higher rank of crash risk")
+# white border outside of boxes
 p = d %>% 
   ggplot(aes(x = path, y = `stats model`, fill = `risk rank`)) + 
   scale_fill_viridis(option="magma", 
                      breaks = c(1, 5, 10, 15, 20), 
                      guide = guide_legend(reverse = FALSE)) + 
-  geom_raster() + 
+  geom_tile(color = "white") + 
   xlab("Path: node A - node Z") + 
   theme_tufte(ticks = F) + 
   theme(axis.title.y = element_blank(),
@@ -38,27 +39,28 @@ p = d %>%
  
 p
 
-ggsave("Figures/concordance_4_models.pdf", p, width = 10*0.8, height = 4*0.8)
+ggsave("Figures/concordance_4_models_white_border.pdf", p, width = 10*0.8, height = 4*0.8)
 
 
 
-
-# PNG
-p = d %>% 
+# no border
+p1 = d %>% 
   ggplot(aes(x = path, y = `stats model`, fill = `risk rank`)) + 
   scale_fill_viridis(option="magma", 
                      breaks = c(1, 5, 10, 15, 20), 
                      guide = guide_legend(reverse = FALSE)) + 
-  #scale_x_continuous(expand = c(0, 0), limits = c(0, 21)) + 
-  geom_raster() + 
+  geom_tile() + 
   xlab("Path: node A - node Z") + 
   theme_tufte(ticks = F) + 
-  theme(text = element_text(size = 35),# need to set font size for pixel figures
-        axis.title.y = element_blank(),
+  theme(axis.title.y = element_blank(),
         axis.title.x = element_text(hjust = 1),
         panel.grid = element_blank(),
         panel.border = element_blank(),
         axis.text.x = element_text(margin = margin(t = -2, r = 0, b = 0, l = 0)),
         axis.text.y = element_text(margin = margin(t = 0, r = -3, b = 0, l = 0),
-                                   hjust = 0))
-ggsave("Figures/concordance_4_models.png", p, width = 10*0.8, height = 4*0.8, dpi = 300)
+                                   hjust = 0)) 
+
+p1
+
+ggsave("Figures/concordance_4_models.pdf", p1, width = 10*0.8, height = 4*0.8)
+
