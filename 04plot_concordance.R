0a3408cd7327a5f97b3d65d1903b61365ed0e934
caimiao0714 <miao.cai@slu.edu>
2019-12-20 00:03:24 -0600

    add model performance metrics

---
+++ 04plot_concordance.R
@@ -0,0 +1,64 @@
+pacman::p_load(dplyr, ggplot2, data.table, tidyr, ggthemes, viridis, showtext)
+showtext_auto()
+
+d = data.table::fread("data/lk_risks.csv") %>% 
+  mutate(path = paste(`Node A`, `Node Z`, sep = "-") %>% 
+           gsub("node ", "", .)) %>% 
+  select(path, starts_with("risk_")) %>% 
+  mutate_each(function(x)dense_rank(desc(x)), -path) %>% 
+  arrange(risk_logit) %>% 
+  mutate(path = factor(path, levels = .$path)) %>% 
+  pivot_longer(cols = risk_logit:risk_DL, 
+               names_to = "stats model", 
+               values_to = "risk rank") %>% 
+  mutate(`stats model` = gsub("risk_", "", `stats model`)) %>% 
+  mutate(`stats model` = case_when(`stats model` == "logit" ~ "Logistic",
+                                   `stats model` == "poisson" ~ "Poisson",
+                                   `stats model` == "xgboost" ~ "XGBoost",
+                                   `stats model` == "DL" ~ "Deep Learning") %>% 
+           factor(levels = c("Logistic", "Poisson", "XGBoost", "Deep Learning")))
+
+#title = "Concordance of four models for evaluating the risk of crash",
+#caption = "Darker color indicates higher rank of crash risk")
+p = d %>% 
+  ggplot(aes(x = path, y = `stats model`, fill = `risk rank`)) + 
+  scale_fill_viridis(option="magma", 
+                     breaks = c(1, 5, 10, 15, 20), 
+                     guide = guide_legend(reverse = FALSE)) + 
+  geom_raster() + 
+  xlab("Path: node A - node Z") + 
+  theme_tufte(ticks = F) + 
+  theme(axis.title.y = element_blank(),
+        axis.title.x = element_text(hjust = 1),
+        panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.text.x = element_text(margin = margin(t = -2, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(margin = margin(t = 0, r = -3, b = 0, l = 0),
+                                   hjust = 0)) 
+ 
+p
+
+ggsave("Figures/concordance_4_models.pdf", p, width = 10*0.8, height = 4*0.8)
+
+
+
+
+# PNG
+p = d %>% 
+  ggplot(aes(x = path, y = `stats model`, fill = `risk rank`)) + 
+  scale_fill_viridis(option="magma", 
+                     breaks = c(1, 5, 10, 15, 20), 
+                     guide = guide_legend(reverse = FALSE)) + 
+  #scale_x_continuous(expand = c(0, 0), limits = c(0, 21)) + 
+  geom_raster() + 
+  xlab("Path: node A - node Z") + 
+  theme_tufte(ticks = F) + 
+  theme(text = element_text(size = 35),# need to set font size for pixel figures
+        axis.title.y = element_blank(),
+        axis.title.x = element_text(hjust = 1),
+        panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.text.x = element_text(margin = margin(t = -2, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(margin = margin(t = 0, r = -3, b = 0, l = 0),
+                                   hjust = 0))
+ggsave("Figures/concordance_4_models.png", p, width = 10*0.8, height = 4*0.8, dpi = 300)
